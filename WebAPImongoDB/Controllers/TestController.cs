7c70e4db365e0182eed5ae679ffded195e088f10
Danh Nguyễn <nguyentd92@gmail.com>
2019-08-20 11:51:26 +0700

    Update

---
+++ WebAPImongoDB/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPImongoDB.Models;
+
+namespace WebAPImongoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Json(new TestModel()
+            {
+                Id = "1",
+                Title = "Noi dung"
+            });
+        }
+
+        [HttpGet("check")]
+        public JsonResult Check()
+        {
+            return Json(new TestModel()
+            {
+                Id = "2",
+                Title = "Noi dung Test"
+            });
+        }
+    }
+}
---
+++ WebAPImongoDB/Models/TestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPImongoDB.Models
+{
+    public class TestModel
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
